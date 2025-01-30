f46a0ebe722a1173dfea2a1f156f1485b8c0a0b1
themickymix <micz.yalon@gmail.com>
2025-01-30 21:19:24 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ app.use(express.urlencoded({ extended: true }));
 //test lang
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: [
